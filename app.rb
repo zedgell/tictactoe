ff910aca4ce11c4a2a0d534689dc2f866fc3ffdd
zedgell <37593687+zedgell@users.noreply.github.com>
2018-04-24 15:00:11 -0400

    needs work

---
+++ app.rb
@@ -0,0 +1,42 @@
+require 'sinatra/base'
+require_relative 'game'
+require_relative 'game_3x3'
+require_relative 'game_4x4'
+require 'sinatra'
+
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  post '/game' do
+    opts = {
+      size: params[:size],
+      mode: params[:mode]
+    }
+    @game = Game.new(opts)
+    session['game'] = @game.session_hash
+    erb :game
+  end
+
+  post '/game/move' do
+    @game = Game.new(session['game'])
+    @player_move = params[:grid_position]
+    @game.round(@player_move)
+
+    if @game.result
+      session['result'] = @game.result
+      redirect '/game/result'
+    else
+      session['game'] = @game.session_hash
+      erb :game
+    end
+  end
+
+  # TODO: Add 'New Game' button to this page
+  get '/game/result' do
+    @result = session['result']
+    @game = Game.new(session['game'])
+    erb :result
+  end
---
+++ game.rb
@@ -0,0 +1,60 @@
+require_relative 'game_3x3.rb'
+require_relative 'game_4x4.rb'
+
+class Game
+
+  attr_accessor :grid, :player_1, :player_2, :cpu, :mode, :size, :turn, :message, :result
+
+  def initialize(opts)
+    @size     = opts[:size]
+    @mode     = opts[:mode]
+    @turn     = opts[:turn]
+    @grid     = opts[:grid] || get_grid(@size)
+    @player_1 = 'X'
+    @player_2 = 'O'
+    @cpu      = 'O'
+    @result   = nil
+    @message  = opts[:message] || 'Tic-tac-toe by zachary edgell'
+  end
+
+  def round(position)
+    game     = game_class.new(session_hash)
+    game.round(position)
+    @grid    = game.grid
+    @mode    = game.mode
+    @size    = game.size
+    @turn    = game.turn
+    @message = game.message
+    @result  = game.result
+  end
+
+  def session_hash
+    {
+      size: @size,
+      mode: @mode,
+      turn: @turn,
+      message: @message,
+      result: @result,
+      grid: @grid
+    }
+  end
+
+  private
+
+  def game_class
+    if @size == '4x4'
+      Game4x4
+    else
+      Game3x3
+    end
+  end
+
+  def get_grid(size)
+    if size == '4x4'
+      Game4x4::GRID
+    else
+      Game3x3::GRID
+    end
+  end
+
+end
